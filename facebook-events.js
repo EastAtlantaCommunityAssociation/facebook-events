540453bd2294ed53cbab2c23139e941f6d70cc98
Justin Burris <justin@groundfloor.us>
2017-11-12 12:22:10 -0500

    Break events into upcoming and past calendar sections

--- facebook-events.js
+++ facebook-events.js
@@ -1,40 +1,81 @@
 $(function() {
-  eventCal = $('#fbEventCalendar ul');
-  if (eventCal.length > 0) {
-    function eventHtml(event) {
-      var date = moment(event.start_time),
-      date_mon = date.format('MMM'),
-      date_day = date.format('Do'),
-      description = event.description.substring(0, 250) + '...',
-      url = 'https://facebook.com/' + event.id;
+  $eventCal = $('#fbEventCalendar');
+  if ($eventCal.length == 0) { return }
 
-      html = '<li class="event">';
+  $upcomingCal = $('#fbUpcomingCalendar ul', $eventCal);
+  $pastCal = $('#fbPastCalendar ul', $eventCal);
 
-      html += '<div class="date">';
-      html += '<div class="date-mon">' + date_mon + '</div>';
-      html += '<div class="date-day">' + date_day + '</div>';
-      html += '</div>';
+  function eventHtml(event) {
+    html = '<li class="event">';
 
-      html += '<div class="details">';
-      html += '<div class="name"><a href="' + url + '">' + event.name + '</a></div>';
-      html += '<div class="location"><a href="' + url + '">' + event.place.name + '</a></div>';
-      html += '<div class="description">' + description + '</div>';
-      html += '</div>';
+    html += '<div class="date">';
+    html += '<div class="date-mon">' + event.month + '</div>';
+    html += '<div class="date-day">' + event.day + '</div>';
+    html += '</div>';
 
-      html += '</li>';
+    html += '<div class="details">';
+    html += '<div class="name"><a target="_blank" href="' + event.url + '">' + event.name + '</a></div>';
+    html += '<div class="location"><a target="_blank" href="' + event.url + '">' + event.location + '</a></div>';
+    html += '<div class="description">' + event.description + '</div>';
+    html += '</div>';
 
-      return html;
-    }
-    $.ajax({
-      url: 'https://zbyaydlemg.execute-api.us-east-1.amazonaws.com/prod/fetch-eaca-facebook-events',
-      type: 'GET',
-      crossDomain: true,
-      success: function(response) {
-        for(var i =0; i < response.length; i++) {
-          event = response[i];
-          eventCal.append(eventHtml(event));
-        }
-      }
-    });
+    html += '</li>';
+
+    return html;
+  };
+
+  function parseFbEvent(rawEvent) {
+    mStart = moment(rawEvent.start_time)
+    return {
+      startTime: mStart,
+      month: mStart.format('MMM'),
+      day: mStart.format('Do'),
+      url: 'https://facebook.com/' + rawEvent.id,
+      description: rawEvent.description.substring(0, 250) + '...',
+      name: rawEvent.name,
+      location: rawEvent.place.name
+    };
   }
+
+  function fbEventManager(events) {
+    var upcomingEvents = [],
+      pastEvents = [];
+
+    // Parse raw fb events and place them into the right upcoming/past boxes
+    for(var i = 0; i < events.length; i++) {
+      fbEvent = parseFbEvent(events[i]);
+
+      if(moment().diff(fbEvent.startTime, 'days') <= 0) {
+        upcomingEvents.push(fbEvent);
+      } else {
+        pastEvents.push(fbEvent);
+      }
+    };
+
+    upcomingEvents.reverse();
+
+    // Clear out loading message
+    $upcomingCal.html('');
+    $pastCal.html('');
+    // Update the DOM with the upcoming events
+    for(var i = 0; i < upcomingEvents.length; i++) {
+      var event = upcomingEvents[i];
+      $upcomingCal.append(eventHtml(event));
+    }
+
+    // Update the DOM with the past events
+    for(var i = 0; i < pastEvents.length; i++) {
+      var event = pastEvents[i];
+      $pastCal.append(eventHtml(event));
+    }
+  };
+
+  $.ajax({
+    url: 'https://zbyaydlemg.execute-api.us-east-1.amazonaws.com/prod/fetch-eaca-facebook-events',
+    type: 'GET',
+    crossDomain: true,
+    success: function(response) {
+      fbEventManager(response);
+    }
+  });
 });
